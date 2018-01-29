07cbc46e1ed883f4d39c1a423680fc65116a46c4
Thanh Long <long25vn@gmail.com>
2018-01-30 03:01:47 +0700

    update

---
+++ go/go_api/api/vendor/database/init.go
@@ -0,0 +1,58 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"time"
+)
+
+var DB *gorm.DB
+var err error
+
+type Post struct {
+	gorm.Model
+	Author  string
+	Book string
+}
+
+func addDatabase(dbname string) error {
+	// create database with dbname, won't do anything if db already exists
+	DB.Exec("CREATE DATABASE " + dbname)
+
+	// connect to newly created DB (now has dbname param)
+	connectionParams := "dbname=" + dbname + " user=docker password=docker sslmode=disable host=db"
+	DB, err = gorm.Open("postgres", connectionParams)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Init() (*gorm.DB, error) {
+	// set up DB connection and then attempt to connect 5 times over 25 seconds
+	connectionParams := "user=docker password=docker sslmode=disable host=db"
+	for i := 0; i < 5; i++ {
+		DB, err = gorm.Open("postgres", connectionParams) // gorm checks Ping on Open
+		if err == nil {
+			break
+		}
+		time.Sleep(5 * time.Second)
+	}
+
+	if err != nil {
+		return DB, err
+	}
+
+	// create table if it does not exist
+	if !DB.HasTable(&Post{}) {
+		DB.CreateTable(&Post{})
+	}
+
+	testPost := Post{Author: "TheName", Book: "TheTitle"}
+	DB.Create(&testPost)
+	testPost = Post{Author: "TheName2", Book: "TheTitle2"}
+	DB.Create(&testPost)
+
+	return DB, err
+}
